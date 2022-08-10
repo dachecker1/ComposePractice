9bc19177909aed42cacfd80085f0d9ecec2be131
andrenivisnk <andrenivinsk@gmail.com>
2022-08-10 22:17:28 +0300

    создал карточку с картинкой и двумя строками

--- app/src/main/java/com/ruyou/composetest/MainActivity.kt
+++ app/src/main/java/com/ruyou/composetest/MainActivity.kt
@@ -3,47 +3,61 @@ package com.ruyou.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5F)
-            ) {
-                Column(
+            Column() {
+                ListItem(name = "naasdfasdfme", prof = "prof")
+                ListItem(name = "naasdfasdfme", prof = "prof")
+                ListItem(name = "naasdfasdfme", prof = "prof")
+                ListItem(name = "naasdfasdfme", prof = "prof")
+                ListItem(name = "naasdfasdfme", prof = "prof")
+                ListItem(name = "naasdfasdfme", prof = "prof")
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 10.dp
+    ) {
+        Box() {
+            Row(verticalAlignment =  Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .background(Color.LightGray)
-                        .fillMaxWidth(0.5F)
-                        .fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "hello!")
-                    Text(text = "Vena")
-                    Text(text = "Madrid")
+                        .size(64.dp)
+                        .clip(shape = CircleShape)
+                        .padding(5.dp)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
                 }
-                Column(
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .fillMaxHeight(0.4F)
-                        .fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "123!")
-                    Text(text = "456")
-                    Text(text = "789")
-                }
+
             }
         }
     }
