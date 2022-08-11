3b9b747af9067c33f3db40b991128265f6ca2b16
andrenivisnk <andrenivinsk@gmail.com>
2022-08-11 10:46:07 +0300

    create button and savingState

--- app/src/main/java/com/ruyou/composetest/MainActivity.kt
+++ app/src/main/java/com/ruyou/composetest/MainActivity.kt
@@ -1,76 +1,64 @@
 package com.ruyou.composetest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.fillMaxSize()) {
-
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircleItem()
             }
         }
     }
 }
 
+@Preview
 @Composable
-private fun ListItem(name: String, prof: String) {
-    var counter = remember {
+private fun CircleItem() {
+    val counter = remember {
         mutableStateOf(0)
     }
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(10.dp)
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+    Box(modifier = Modifier
+        .background(color = color.value, shape = CircleShape)
+        .size(100.dp)
         .clickable {
             counter.value++
-        },
-        shape = RoundedCornerShape(15.dp),
-        elevation = 10.dp
-    ) {
-        Box() {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    painter = painterResource(id = R.drawable.image),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(64.dp)
-                        .clip(shape = CircleShape)
-                        .padding(5.dp)
-                )
-                Column(modifier = Modifier.padding(start = 16.dp)) {
-                    Text(text = counter.value.toString())
-                    Text(text = prof)
-                }
-
+            if (counter.value > 10) {
+                color.value = Color.Red
             }
-        }
+        },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = counter.value.toString(),
+            style = TextStyle(color = Color.White, fontSize = 26.sp)
+        )
     }
 }
 
